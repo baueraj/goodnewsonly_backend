52d55f29741ba1f91111efd36ca32e0f3cad0f19
abauer <abauer@seekr.com>
2023-10-05 20:51:22 -0700

    Updates to debug, add linters

--- goodnewsonly/serve.py
+++ goodnewsonly/serve.py
@@ -1,9 +1,25 @@
+import logging
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .api_models import AnalysisRequest
 from .core import process_website
 
-app = FastAPI()
+HOSTNAME = os.environ.get("HOSTNAME", "unknown")
+
+log_level = os.getenv("LOG_LEVEL", default="info")
+log_level_map = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+}
+print(f"Setting log level to {log_level}")
+LOGGING_FORMAT = "[%(asctime)s.%(msecs)03d][%(levelname)s ][%(module)s.%(funcName)s ][%(threadName)s ] - %(message)s"
+logging.basicConfig(format=LOGGING_FORMAT, level=log_level_map[log_level], datefmt="%Y-%m-%d %H:%M:%S")
+logger = logging.getLogger(__name__)
+
+app = FastAPI(title="GoodNewsOnly")
 
 # CORS middleware settings
 app.add_middleware(
@@ -13,6 +29,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, world!"}
 
 
 @app.post("/analyze")
