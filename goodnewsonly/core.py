fb8847f27313b30ec69a9082fa270eba00084616
AndrewBauer <abauer@seekr.com>
2023-10-09 16:41:06 -0700

    Misc changes

--- goodnewsonly/core.py
+++ goodnewsonly/core.py
@@ -6,7 +6,7 @@ from .sentiment_analysis import SentimentAnalyzer
 analyzer = SentimentAnalyzer()
 
 
-def process_website(url: str) -> list:
+def process_website(url: str) -> List[str]:
     headlines = extract_headlines(url)
     sentiments = analyzer.analyze_sentiments(headlines)
     negative_headlines = [headline for headline, sentiment in zip(headlines, sentiments) if sentiment == "negative"]
